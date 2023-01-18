f03a5ce9e86b22f9214c6c4da9b67d62d155ab58
pythonkila <motolaoshin@hotmail.com>
2023-01-18 01:02:36 -0600

    bug fix

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+# fix bug in application.java
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
